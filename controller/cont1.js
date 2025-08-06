deba4d07ea65ee2c580cac5a4e356aeb79b7e6d7
DipeshOmre <dipesh@gmail.com>
2025-08-06 19:44:19 +0530

    feat: implement add home functionality and enhance home listing page

--- controller/cont1.js
+++ controller/cont1.js
@@ -1,19 +1,19 @@
 const Home = require("../model/home");
 const getaddhome = (req, res, next) => {
     // res.sendFile(path.join(rootDir,'views','addhome.html'));
-    res.render("addhome", { pageTitle: 'form page' })
+    res.render("host/addhome", { pageTitle: 'form page' })
 }
 exports.postaddhome = (req, res, next) => {
     const { homeName, location, price, rating } = req.body;
     const h = new Home(homeName, location, price, rating);
     h.save();
     // res.sendFile(path.join(rootDir,'views','homeAdded.html'));
-    res.render("homeAdded", { pageTitle: "Succes page" })
+    res.render("host/homeAdded", { pageTitle: "Succes page" })
 
 }
 exports.gethome = (req, res, next) => {
     Home.fetchAll((registeredHomes) => {
-        res.render('home', { registeredHomes: registeredHomes, pageTitle: 'Main page' })
+        res.render('store/home-list', { registeredHomes: registeredHomes, pageTitle: 'Main page' })
     });
 
     // res.send("<h1>Welcome to Airbnb</h1><a href='/host/add-home'>Add Home</a>");
